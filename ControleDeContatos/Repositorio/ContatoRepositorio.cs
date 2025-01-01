fd83889da54846637ef652d479e2ef4abf3b5551
laryssalorene <laryssalo@edu.unifor.br>
2025-01-01 19:00:34 -0300

    adicionando alterações

---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio :IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+
+        public ContatoRepositorio(BancoContext bancoContext) 
+        {
+            _bancoContext = bancoContext;
+        
+        }
+        public List<ContatoModel> BuscarTodos()
+        {
+            return _bancoContext.Contatos.ToList();
+        }
+
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            //gravar no banco de dados; quem grava propriamente dito é o contexto q tá dentro de Data
+            //logo, este contexto precisa ser injetado aqui no ContatoRepositorio
+            //essa injeção só pode ocorrer por meio do construtor
+
+            _bancoContext.Add(contato);
+            _bancoContext.SaveChanges();
+
+            return contato;
+
+
+        }
+    }
+}
