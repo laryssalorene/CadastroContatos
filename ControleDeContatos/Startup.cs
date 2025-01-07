b948ea56a333f3772e90449a61dc609ac783793c
Laryssa <laryssalo@edu.unifor.br>
2025-01-07 03:33:48 -0300

    Finalizando Crud Contato

---
+++ ControleDeContatos/Startup.cs
@@ -0,0 +1,59 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ControleDeContatos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<BancoContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DataBase")));
+
+            //Toda vez q a interface IContatoRepositorio for chamada,
+            //a injeção de dependendica logo abaixo vai, automaticamente, pegar e usar tudo da ContatoRepositorio
+            services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)  // Mudança aqui: IHostingEnvironment foi descontinuado, use IWebHostEnvironment
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Definindo a rota padrão
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });  // Adicionada a vírgula aqui
+        }
+    }
+}
